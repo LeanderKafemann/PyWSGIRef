71e96ad977cb6272741101e4dd34be21f67dbed6
leander@kafemann.berlin <leander@kafemann.berlin>
2025-06-07 16:59:46 +0200

    add code sample

---
+++ src/pywsgiref/__init__.py
@@ -0,0 +1,9 @@
+"""
+PYWSGIREF
+not ready yet!
+"""
+def about():
+    """
+    Returns information about your release and other projects by LK
+    """
+    return {"Version":(1, 0, 0), "Author":"Leander Kafemann", "date":"07.06.2025", "recommend":("Büro by LK",  "pyimager by LK", "naturalsize by LK"), "feedbackTo": "leander@kafemann.berlin"}
