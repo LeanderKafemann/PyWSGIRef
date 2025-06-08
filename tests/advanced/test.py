d9e20b01711cb57889fd9ecb5661e1f1eec4aeb3
leander@kafemann.berlin <leander@kafemann.berlin>
2025-06-08 18:32:30 +0200

    add advanced test

---
+++ tests/advanced/test.py
@@ -0,0 +1,21 @@
+from PyWSGIRef import *
+
+addSchablone("helloWorld", loadFromFile("./shortcutHelloWorld.pyhtml"))
+
+def contentGeneratingFunction(path: str) -> str:
+    """
+    A simple content generating function that returns a greeting.
+    """
+    match path:
+        case "/":
+            return MAIN_HTML.format(about()["Version"])
+        case "/hello":
+            return SCHABLONEN["helloWorld"].decoded()
+        case _:
+            return "404 Not Found"
+
+application = makeApplicationObject(contentGeneratingFunction)
+server = setUpServer(application)
+
+print("Successfully started WSGI server on port 8000.")
+server.serve_forever()
