c852aa9c9f8a4e1e203296e1a9ce6876c63e0bf8
leander@kafemann.berlin <leander@kafemann.berlin>
2025-06-08 12:07:40 +0200

    merge main method

--- src/PyWSGIRef/defaults.py
+++ src/PyWSGIRef/defaults.py
@@ -12,3 +12,17 @@ ERROR = """<!DOCTYPE html>
         <p>Es ist ein Fehler aufgetreten.</p>
     </body>
 </html>"""
+
+MAIN_HTML = """<!DOCTYPE html>
+    <html lang="de">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>PyWSGIRef Server</title>
+        </head>
+        <body>
+            <h1>Willkommen zum PyWSGIRef Server!</h1>
+            <p>Dies ist die Hauptseite des Servers.</p>
+            <p>Sie nutzen Version {} von PyWSGIRef.</p>
+        </body>
+    </html>"""
